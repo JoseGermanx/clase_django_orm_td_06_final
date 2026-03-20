d819eedf09168c117318f1480fb27e6d6257457f
José Martínez <jgermanmm@gmail.com>
2026-03-20 20:50:25 -0300

    messages app

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Cliente
+from django.contrib import messages
 
 from django.urls import reverse_lazy
 
@@ -17,13 +18,27 @@ class ClienteCreateView(CreateView):
     fields = ['nombre', 'apellido', 'edad', 'correo', 'telefono', 'activo']
     success_url = reverse_lazy('cliente_list')
 
+    def form_valid(self, form):
+        messages.success(self.request,"Cliente creado correctamente.")
+        return super().form_valid(form)
+
 class ClienteUpdateView(UpdateView):
     model = Cliente
     template_name = 'clientes/cliente_form.html'
     fields = ['nombre', 'apellido', 'edad', 'correo', 'telefono', 'activo']
     success_url = reverse_lazy('cliente_list')
 
+    def form_valid(self, form):
+        messages.success(self.request,"Cliente actualizado correctamente.")
+        return super().form_valid(form)
+
 class ClienteDeleteView(DeleteView):
     model = Cliente
     template_name = 'clientes/cliente_confirm_delete.html'
     success_url = reverse_lazy('cliente_list')
+
+    def post(self, request, *args, **kwargs):
+        messages.success(self.request, "Cliente eliminado correctamente.")
+        return super().post(request, *args, **kwargs)
+
+       
